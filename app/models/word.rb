b7c42ce42f735618246864c404bc524a77a50627
blnr <belanger.kyle@gmail.com>
2013-12-21 19:44:28 -0500

    Add working search to display definitions

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,27 +1,24 @@
-class Word
-  
-  require 'rubygems'
-  require 'json'
+  class Word
+    
+    require 'rubygems'
+    require 'json'
 
-  attr_accessor :search 
+    attr_accessor :search 
 
- 
-  def self.search(search)
+    def self.search(search)
 
-    # Get back JSON response containing array of hashes?
-    response = Wordnik.word.get_definitions(search)
+      # Get back JSON response containing array of hashes?
+      response = Wordnik.word.get_definitions(search)
 
-    # Parse JSON to an array of hashes?
-    hash_result = JSON.parse(response)
+      # Access first hash in the array
+      #result = response.first
 
-    # Access first hash in the array
-    #result = hash_result.first
 
-    #words_json = JSON.parse(words_hash.to_json) 
+      #words_json = JSON.parse(words_hash.to_json) 
 
-    #return :text key of the hash
-    #definition = words_json["text"].to_s
+      #return :text key of the hash
+      #definition = words_json["text"].to_s
 
-  end  
+    end  
 
-end
+  end
