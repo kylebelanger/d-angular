3237f522c3310a3b14309237e8d60bb72981a2ad
blnr <belanger.kyle@gmail.com>
2014-06-13 13:14:26 -0400

    heroku patch

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Diction
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Herok push fix (06/13/14)
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
