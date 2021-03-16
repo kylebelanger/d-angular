35c119f24674b141ffe607030753c08be59916cc
Kyle Belanger <kyle.belanger@caci.com>
2021-03-16 14:46:16 -0400

    updates

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ angular.module('d-angular', ['ui.router'])
 
 				// Home state
 				.state('home', {
-				  url: 'home',
-				  templateUrl: 'static/home.html',
+				  url: '/home',
+				  templateUrl: '../static/home.html',
 				  controller: 'MainCtrl'
 				})
 		}
